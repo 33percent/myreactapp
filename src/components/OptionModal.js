c126940c6e5bc69ea9f30decd196d59828f50ee1
sandeep0814 <sandeep0814@gmail.com>
2018-06-04 01:05:19 +0530

    modbile responsivensss

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,10 +7,12 @@ const OptionModal = (props) => (
 <Modal
 isOpen={!!props.selectedOption}
 onRequestClose={props.handleclearer}
+closeTimeoutMS={200}
+className="modal"
 contentLabel="Selected Option">
-    <h3>Select Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleclearer}>
+    <h3 className="modal__title">Select Option</h3>
+    {props.selectedOption && <p className="modal_body">{props.selectedOption}</p>}
+    <button className="button" onClick={props.handleclearer}>
         Okay
     </button>
 </Modal>
