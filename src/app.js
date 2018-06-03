ecf0b21390853041873124624b7ffc207a0cd335
sandeep0814 <sandeep0814@gmail.com>
2018-06-03 15:42:51 +0530

    class trasform for avoiding constructor binding

--- src/app.js
+++ src/app.js
@@ -1,10 +1,34 @@
-// Grab the add function from the add.js file in the utils folder
-// Grab React from the react npm module
-// add(2, 4)
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import IndecisionApp from './components/IndecisionApp';
 
-  ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+class myold {
+  constructor(){
+    this.name="old",
+    this.callme = this.callme.bind(this)
+  };
+    
+  callme(){
+    return `HI My name is ${this.name}`;
+  }
+}
+
+const myname = new myold();
+const callme = myname.callme;
+console.log(callme())
+console.log(myname.callme())
+
+class newold {
+name = "sandeep";
+newgreeting = () =>{
+  return `Hi My name is ${this.name}`;
+}
+};
+
+const newname = new newold();
+const newcaling = newname.newgreeting;
+console.log(newcaling())
+console.log(newname)
