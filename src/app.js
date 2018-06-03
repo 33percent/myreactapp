3e48ffcf13aa27b5c3bf18eefcdabe2fcc025b01
sandeep0814 <sandeep0814@gmail.com>
2018-06-03 22:27:55 +0530

    configuring sass in webpack

--- src/app.js
+++ src/app.js
@@ -2,33 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import IndecisionApp from './components/IndecisionApp';
-
+import './styles/style.scss';
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
-
-// class myold {
-//   constructor(){
-//     this.name="old",
-//     this.callme = this.callme.bind(this)
-//   };
-    
-//   callme(){
-//     return `HI My name is ${this.name}`;
-//   }
-// }
-
-// const myname = new myold();
-// const callme = myname.callme;
-// console.log(callme())
-// console.log(myname.callme())
-
-// class newold {
-// name = "sandeep";
-// newgreeting = () =>{
-//   return `Hi My name is ${this.name}`;
-// }
-// };
-
-// const newname = new newold();
-// const newcaling = newname.newgreeting;
-// console.log(newcaling())
-// console.log(newname)
